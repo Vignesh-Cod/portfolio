6358d98206547bf71f3e3cf725cbb66ea75986b2
Vignesh-Cod <venkatvignesh72@gmail.com>
2025-03-21 11:25:51 +0530

    link added

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -1,6 +1,7 @@
 import HeroImg from  '../assets/hero.png';
-import { AiOutlineTwitter ,AiOutlineFacebook,AiOutlineLinkedin } from "react-icons/ai";
-
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { BiLogoGmail } from "react-icons/bi";
+import { FaGithub,FaInstagram } from "react-icons/fa";
 export default function Hero(){
     return <section id='Hero' className='flex flex-col md:flex-row px-5  py-32 bg-primary justify-center'>
       
@@ -9,13 +10,13 @@ export default function Hero(){
         <p className='text-2xl'>Im a Full-Stack developer</p>
         </h1>
         <div className='flex py-10'>
-            <a href='' className='pr-5 hover:text-white'><AiOutlineTwitter size={40}/></a>
-            <a href='' className='pr-5 hover:text-white'><AiOutlineFacebook size={40}/></a>
-            <a href='' className='hover:text-white'><AiOutlineLinkedin size={40}/></a>
+            <a href="mailto:venkatvignesh72@gmail.com" className='pr-5 hover:text-white'><BiLogoGmail size={40} />
+            <a href='https://github.com/Vignesh-Cod' className='pr-5 hover:text-white'><FaGithub  size={40}/></a>
+            <a href='https://www.linkedin.com/in/venkatvignesh07/' className='hover:text-white'><AiOutlineLinkedin size={40}/></a>
+            <a href='https://www.instagram.com/suveen_003/' className='hover:text-white'><FaInstagram size={40} />
         </div>
         </div>
         <img className='md:w-1/3' src={HeroImg}></img>
 
     </section>
-
 }
